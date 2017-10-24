880e2762fc3622ef558e83cef48a4518b8fa35f2
niko <ktotutus@gmail.com>
2017-10-24 23:32:23 +0300

    adding docker-compose

--- setup_raspbian.sh
+++ setup_raspbian.sh
@@ -3,6 +3,9 @@ passwd
 sudo apt-get update
 sudo apt-get upgrade -y
 curl -sSL https://get.docker.com | sh
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo usermod -aG docker pi
-sudo apt-get install vim python3-rpi.gpio ipython3 -y
+sudo apt-get install vim python3-rpi.gpio ipython3 python-pip  -y
+pip install pip -U
+sudo pip install docker-compose
 
