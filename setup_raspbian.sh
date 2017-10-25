449fa88f1e4d6c661aefdb42b6a2b34f76b9abb6
Your Name <you@example.com>
2017-10-25 20:46:25 +0000

    update configs

--- setup_raspbian.sh
+++ setup_raspbian.sh
@@ -16,49 +16,14 @@ sudo apt-get install graphite-carbon
 sudo apt-get install python python-pip build-essential python-dev libcairo2-dev libffi-dev
 sudo pip install graphite-api
 
-# /etc/graphite-api.yml
-# search_index: /var/lib/graphite/index
-# finders:
-  # - graphite_api.finders.whisper.WhisperFinder
-# functions:
-  # - graphite_api.functions.SeriesFunctions
-  # - graphite_api.functions.PieFunctions
-# whisper:
-  # directories:
-    # - /var/lib/graphite/whisper
-# carbon:
-  # hosts:
-    # - 127.0.0.1:7002
-  # timeout: 1
-  # retry_delay: 15
-  # carbon_prefix: carbon
-  # replication_factor: 1
 
-apt-get install libapache2-mod-wsgi
+#sudo apt-get install apache2 libapache2-mod-wsgi
 
-# /var/www/wsgi-scripts/graphite-api.wsgi
-# from graphite_api.app import app as application
+sudo unlink /etc/apache2/sites-enabled/000-default.conf
+sudo ln -s /home/pi/raspberry/graphite.conf /etc/apache2/sites-enabled/graphite.conf
+sudo ln -s /home/pi/raspberry/graphite-api.yml /etc/graphite-api.yml
+sudo ln -s /home/pi/rasberry/graphite_api /var/www/graphite_api
+sudo a2enwsgi
+sudo a2enmod headers
 
-/etc/apache2/sites-available/graphite.conf
-
-# # /etc/apache2/sites-available/graphite.conf
-# LoadModule wsgi_module modules/mod_wsgi.so
-# WSGISocketPrefix /var/run/wsgi
-# Listen 8013
-# <VirtualHost *:8013>
-
- # WSGIDaemonProcess graphite-api processes=5 threads=5 display-name='%{GROUP}' inactivity-timeout=120
- # WSGIProcessGroup graphite-api
- # WSGIApplicationGroup %{GLOBAL}
- # WSGIImportScript /var/www/wsgi-scripts/graphite-api.wsgi process-group=graphite-api application-group=%{GLOBAL}
-
- # WSGIScriptAlias / /var/www/wsgi-scripts/graphite-api.wsgi
-
- # <Directory /var/www/wsgi-scripts/>
- # Order deny,allow
- # Allow from all
- # </Directory>
- # </VirtualHost>
-
-ln -s ../sites-available/graphite.conf .
 service apache2 restart
